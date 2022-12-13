b6dbcd286133d129a9433d2d16bf6b9a1c7dbe7f
antonvh <antonhaninvl@gmail.com>
2022-12-13 16:47:34 +0300

    набросал layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,68 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import './styles.less';
+import mathExpressionsParse from './utils/mathExpressionsParse';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [result, setResult] = useState(NaN);
+  const [currentExpression, setCurrentExpression] = useState('');
+  const [mathSymbol, setMathSymbol] = useState(true);
+  
+
+  const updateExpression = (change: string) => () => {
+    setCurrentExpression(`${currentExpression}${change}`) ;
+  }
+
+  const keyboard: { value: string, variant?: 'default' | 'fill' | 'secodary', onClick?():void }[] = [
+    { value: 'AC', onClick: () => setCurrentExpression('') },
+    {
+      value: '+/-',
+      onClick: () => {
+        setMathSymbol(!mathSymbol);
+        setCurrentExpression(`${mathSymbol ? '+' : '-'}(${currentExpression})`);
+      }
+    },
+    { value: '%' },
+    { value: ' / ', variant: 'secodary' },
+    { value: '7' },
+    { value: '8' },
+    { value: '9' },
+    { value: ' * ', variant: 'secodary' },
+    { value: '4' },
+    { value: '5' },
+    { value: '6' },
+    { value: ' - ', variant: 'secodary' },
+    { value: '1' },
+    { value: '2' },
+    { value: '3' },
+    { value: ' + ', variant: 'secodary' },
+    { value: '0' },
+    { value: '.' },
+    { value: '' },
+    { value: '=', variant: 'fill',
+      onClick: () =>  {
+        setResult(mathExpressionsParse(currentExpression));
+      }
+    },
+  ];
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={`layout theme_${'light'}`}>
+        <div className="exeInput">
+          <span>{`${result}` !== 'NaN' ? result : 'Error'}</span>
+          <span>{currentExpression}</span>
+          </div>
+        <div className="calcKeyboard">{
+          keyboard.map(
+            ({ value, variant = 'default',  onClick }) => (
+              <button className={`calcButton calcButton_${variant} ${value ? '' : 'hidden'}`.trimEnd()} onClick={onClick ?? updateExpression(value)}>
+                {value}
+              </button>
+            )
+          )
+        }</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 };
