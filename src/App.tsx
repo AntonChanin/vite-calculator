046163e87ffa20e07f4d0fcd696602ba19cdc6a0
antonvh <antonhaninvl@gmail.com>
2023-01-09 14:03:10 +0300

    поправил логику кнопки +/-

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,54 @@
 import { useState } from 'react';
+
 import './App.css';
 import './styles.less';
 import mathExpressionsParse from './utils/mathExpressionsParse';
 
 function App() {
   const [result, setResult] = useState(NaN);
-  const [currentExpression, setCurrentExpression] = useState('');
+  const [currentExpression, setCurrentExpression] = useState<string[]>([]);
   const [mathSymbol, setMathSymbol] = useState(true);
   
 
   const updateExpression = (change: string) => () => {
-    setCurrentExpression(`${currentExpression}${change}`) ;
+    setCurrentExpression([...currentExpression, change]);
   }
 
   const keyboard: { value: string, variant?: 'default' | 'fill' | 'secodary', onClick?():void }[] = [
-    { value: 'AC', onClick: () => setCurrentExpression('') },
+    { value: 'AC', onClick: () => setCurrentExpression([]) },
     {
       value: '+/-',
       onClick: () => {
         setMathSymbol(!mathSymbol);
-        setCurrentExpression(`${mathSymbol ? '+' : '-'}(${currentExpression})`);
+        const numbersOfExpression = currentExpression
+          .join('')
+          .split('_')
+          .map((value) => ['+', '-', '/', '*', '%'].includes(value) ? ` ${value} ` : value);
+        const lastNumber = numbersOfExpression[numbersOfExpression.length - 1];
+        numbersOfExpression[numbersOfExpression.length - 1] = `${mathSymbol ? '+' : '-'}(${lastNumber})`;
+        setCurrentExpression(numbersOfExpression.map((value) => [' + ', ' - ', ' / ', ' * ', ' % '].includes(value) ? `_${value}_` : value));
       }
     },
-    { value: '%' },
-    { value: ' / ', variant: 'secodary' },
+    { value: '_%_' },
+    { value: '_/_', variant: 'secodary' },
     { value: '7' },
     { value: '8' },
     { value: '9' },
-    { value: ' * ', variant: 'secodary' },
+    { value: '_*_', variant: 'secodary' },
     { value: '4' },
     { value: '5' },
     { value: '6' },
-    { value: ' - ', variant: 'secodary' },
+    { value: '_-_', variant: 'secodary' },
     { value: '1' },
     { value: '2' },
     { value: '3' },
-    { value: ' + ', variant: 'secodary' },
+    { value: '_+_', variant: 'secodary' },
     { value: '0' },
     { value: '.' },
     { value: '' },
     { value: '=', variant: 'fill',
       onClick: () =>  {
-        setResult(mathExpressionsParse(currentExpression));
+        setResult(mathExpressionsParse(currentExpression.join().replaceAll('_', ' ').replaceAll(',', '')));
       }
     },
   ];
@@ -51,17 +58,23 @@ function App() {
       <div className={`layout theme_${'light'}`}>
         <div className="exeInput">
           <span>{`${result}` !== 'NaN' ? result : 'Error'}</span>
-          <span>{currentExpression}</span>
+          <span>{currentExpression.join('').replaceAll('_', ' ')}</span>
           </div>
-        <div className="calcKeyboard">{
-          keyboard.map(
-            ({ value, variant = 'default',  onClick }) => (
-              <button className={`calcButton calcButton_${variant} ${value ? '' : 'hidden'}`.trimEnd()} onClick={onClick ?? updateExpression(value)}>
-                {value}
-              </button>
+        <div className="calcKeyboard">
+          {
+            keyboard.map(
+              ({ value, variant = 'default',  onClick }, index) => (
+                <button
+                  className={`calcButton calcButton_${variant} ${value ? '' : 'hidden'}`.trimEnd()}
+                  onClick={onClick ?? updateExpression(value)}
+                  key={index}
+                >
+                  {value.replaceAll('_', ' ')}
+                </button>
+              )
             )
-          )
-        }</div>
+          }
+        </div>
       </div>
     </div>
   );
